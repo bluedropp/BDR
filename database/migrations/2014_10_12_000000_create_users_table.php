db4785828bdc3c3af2e9fbec43bdd7dfd4276f34
bluedropp <loick.dumoulin@laposte.net>
2020-06-21 19:18:01 +0200

    zesdqsd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
 
-            $table->string('pseudo');
-            $table->string('email',255)->unique();
+            
+            $table->string('email')->unique();
 
             $table->string('name');
             $table->string('prenom');
-            $table->string('email')->unique();
+            
             $table->timestamp('email_verified_at')->nullable();
 
             $table->string('password');
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,5 +97,14 @@
             @yield('content')
         </main>
     </div>
+    <footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            <strong>BDR</strong> by Loick DUMOULIN GREGOIRE, Mostafa KASSEM and Jean-Baptiste SCHNERB. The source code is licenced
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+          </p>
+        </div>
+      </footer>
 </body>
 </html>
